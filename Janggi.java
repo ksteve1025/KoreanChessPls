4ac1e0986b24ae804132e1f67018d524d3fb494d
PolarbearRescue <yamsushi@gmail.com>
2014-06-05 20:12:15 -0700

    ;W;

---
+++ Janggi.java
@@ -0,0 +1,69 @@
+
+/**
+ * Write a description of class Janggi here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+import java.io.*;
+
+public class Janggi
+{
+    private char [][] board; 
+    Janggi()
+    {
+        board = new char [11][10];
+    }
+
+    public void printBoard()
+    {
+        System.out.println("\f");
+        System.out.println("1   2   3   4   5   6   7   8   9");
+        for(int r = 1; r < board.length; r++)
+        {
+            for(int c = 1; c < board[r].length; c++)
+            {
+                if (board [r][c] == 0)
+                    System.out.print(" ");
+                if ( c < board[r].length)
+                    System.out.print(" | ");
+            }
+            System.out.println(r);
+            if (r < board.length-1)
+                System.out.println("--+---+---+---+---+---+---+---+---+---");
+        }
+
+        System.out.println("\nWELCOME TO JANGGI");
+    }
+
+    public void playGame()
+    {
+
+    }
+
+    public void directions()
+    {
+        System.out.println("Janggi, also known as Korean Chess,");
+        System.out.println("is a strategy board game that");
+        System.out.println("derived from xiangqi(Chinese chess).");
+        System.out.println("To move the pieces, type the piece and type in the coordinate.");
+    }
+
+    public void printPieces()
+    {
+        for (int r = 3; r < board.length; r++)
+        {
+            for (int c = 1; c < board[r].length; c++)
+            {
+                
+            }
+        }
+    }
+
+    public boolean removePiece()
+    {
+        return false;
+    }
+
+}
